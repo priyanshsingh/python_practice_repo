1b2c7fdcfb9d3b40bd4c89d6e0c086b97399e492
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-18 22:08:34 +0530

    added type conversion code

--- revision/type_conversion2.py
+++ revision/type_conversion2.py
@@ -3,3 +3,8 @@ second = input("Enter number 2 = ")
 add = float(first) + float(second)
 
 print("Addition is", str(add))
+
+third = float(input("Enter number 3 = "))
+fourth = float(input("Enter number 4 = "))
+add1 = third + fourth
+print("Addition is", str(add1))
