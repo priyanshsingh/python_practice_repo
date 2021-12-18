ae4bfcbe1bdb60bd6a6c825842cca59b413f9ff0
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-18 22:04:28 +0530

    added type conversion code

---
+++ revision/type_conversion.py
@@ -0,0 +1,4 @@
+birth_year = input("Enter your Birth Year? ")
+age = 2021 - int(birth_year)
+print("Your age is = ", end="")
+print(age)
---
+++ revision/type_conversion2.py
@@ -0,0 +1,5 @@
+first = input("Enter number 1 = ")
+second = input("Enter number 2 = ")
+add = float(first) + float(second)
+
+print("Addition is", str(add))
