ea823e3d94bd6d93d5522887862de091eeaff089
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-19 15:02:28 +0530

    added arithmetic operators code

--- revision/arithrmetic_operators.py
+++ revision/arithrmetic_operators.py
@@ -1,5 +1,20 @@
-print(10 + 3)
-print(10 - 3)
+#add
+print(10 + 3)  
+
+#subtract
+print(10 - 3)  
+
+#multiply
 print(10 * 3)
-print(10 / 3)
-print(10 % 3)
+
+#power...10 raised to power 3, i.e. 10^3
+print(10 ** 3) 
+
+#divide
+print(10 / 3)  
+
+#divide and return integer value
+print(10 // 3) 
+
+#modulus
+print(10 % 3)  
