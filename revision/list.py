997c35e94ced621e3c0cc4c69b50b57cdc5a5f7a
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-19 20:23:49 +0530

    added lists

---
+++ revision/list.py
@@ -0,0 +1,6 @@
+names = ["Priyansh", "Harsh", "Parth", "Eshaan", "Aarsh", "Singh"]
+print(names)
+print(names[0])
+print(names[-1])
+print(names[0:4])
+print(names[0] + " " + names [5])
---
+++ revision/while_loop.py
@@ -0,0 +1,15 @@
+i = 1
+while i <= 10:
+    print(i)
+    i+=1
+j = 1
+while j <= 5_0:
+    print(j)
+    j+=1
+
+k = 1
+while k <= 20:
+    print(k * "*")
+    k = k + 1
+
+
