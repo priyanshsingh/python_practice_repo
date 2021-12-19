c08b98e160825cc9819712112be34a340be547d9
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-19 14:57:09 +0530

    added string and arithmetic operators code

---
+++ revision/strings.py
@@ -0,0 +1,9 @@
+course = "Python crash course"
+print(course)
+print(course.upper())
+print(course.lower())
+print(course.find('c'))
+print('Python' in course)
+print(course.replace('crash', 'beginners'))
+
+
