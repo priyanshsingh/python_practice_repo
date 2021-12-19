ee9809f0af26a9567687d09d28cbc1169dbc65cc
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-19 15:37:04 +0530

    added if statements code

---
+++ revision/if_statements.py
@@ -0,0 +1,15 @@
+temperature = 15
+if temperature > 30:
+    print("It's a hot day")
+    print("Drink plenty of water!!!")
+elif temperature > 20 and temperature <31:
+    print("It's a Pleasant day!!!")
+    print("Have FUN!!!")
+    print("Drink plenty of water!!!")
+
+else:
+    print("It's a Cold day!!!")
+    print("Stay Inside!!!")
+
+print(" ")
+print("End of the if else statement")
---
+++ revision/logical_operators.py
@@ -0,0 +1,10 @@
+price = 25
+print(price > 10 and price < 30)
+
+price = 25
+print(price > 10 or price < 15)
+
+price = 25
+print(not price > 5)
+
+
