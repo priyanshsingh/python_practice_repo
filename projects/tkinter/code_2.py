51b58ee350488531ac724b0b658880a9729786db
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-21 02:01:02 +0530

    added minsize maxsize label code in tkinter project

--- projects/tkinter/code_2.py
+++ projects/tkinter/code_2.py
@@ -11,4 +11,6 @@ priyansh_root.minsize(320, 240)
 # width, height --> maxsize function
 priyansh_root.maxsize(900, 720)
 
+varun = Label(text="Hello, this is the first label of my first GUI Window using Tkinter")
+varun.pack()
 priyansh_root.mainloop()
