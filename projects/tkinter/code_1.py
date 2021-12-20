db59de5de679818cc5c71fa689affe614827ea99
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-21 01:55:56 +0530

    added tkinter GUI project beginning codes

---
+++ projects/tkinter/code_1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+#GUI Base built by creating an instance of Tk class
+priyansh_root = Tk()
+
+#GUI LOGIC is to be written in the event loop
+#(i.e. mainloop), which keeps user in the GUI
+#for futher use of the Application
+
+priyansh_root.mainloop()
---
+++ projects/tkinter/code_2.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+priyansh_root = Tk()
+
+# "WidthxHeight" --> for geometry function of the class Tk
+priyansh_root.geometry("640x480")
+
+# width, height --> minsize function
+priyansh_root.minsize(320, 240)
+
+# width, height --> maxsize function
+priyansh_root.maxsize(900, 720)
+
+priyansh_root.mainloop()
