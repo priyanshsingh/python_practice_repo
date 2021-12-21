9fa9b317d5dfb7cfa860ec989081388afe50bed6
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-21 22:46:37 +0530

    added code

---
+++ revision/revision_2.0/7.1_list_method.py
@@ -0,0 +1,6 @@
+numbers = [13, 45, 878, 43, 11, 467]
+print(numbers)
+
+numbers.sort()
+
+print(numbers)
---
+++ revision/revision_2.0/7_lists_and_tuples.py
@@ -0,0 +1,10 @@
+names = ['Priyansh', 'Parth', 'Aarsh', 'Eshaan', 'Harsh']
+print(names)
+print(names[0])
+print(names[0:3])
+d = names.append('Amit')
+print(names)
+a = names.count('Parth')
+
+variable_list = ['Priyansh', 20, True, 793.23]
+print(variable_list)
