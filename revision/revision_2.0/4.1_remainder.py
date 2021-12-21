ebc26995d7de8908e2b7e3435be7f1b390882abe
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-21 18:24:06 +0530

    added practice set questions

---
+++ revision/revision_2.0/4.1_remainder.py
@@ -0,0 +1,6 @@
+a = int(input("Enter an Integer = ")) 
+b = int(input("Enter the Integer by which number 1 is to be divided = "))
+
+c = a%b
+
+print("Result: Remainder is = " + str(c))
---
+++ revision/revision_2.0/4_practice_set.py
@@ -0,0 +1,5 @@
+print("Add Two Numbers: ")
+a = float(input("Enter number one = "))
+b = float(input("Enter number two = "))
+c = a+b
+print("Result: a + b = " + str(c))
