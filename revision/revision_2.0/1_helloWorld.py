7e10ac2f723b5c4d99a499742b4a2710d3d9b799
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-21 14:51:24 +0530

    added playsound and a few revision codes

---
+++ revision/revision_2.0/1_helloWorld.py
@@ -0,0 +1,12 @@
+'''Modules --> A module is a filr containing code written by somebody else(usually)
+which can be imported and used in our programs
+
+PIP --> PIP is a package manager for python. PIP can be used to install a package in the system.
+Eg. pip install pillow --> python imaging library, used to import images for a GUI Program
+
+1. Built-in Modules -->  Already installed in Python, eg. os, abc etc.
+2. External Modules --> Modules that are needed to be installed externally using PIP. Eg. flask, tensorflow etc.
+
+REPL --> Read-Evaluate-Print-Loop(use command "python in the terminal to launch REPL"). '''
+
+print("Hello World")
---
+++ revision/revision_2.0/2.1_playsound.py
@@ -0,0 +1,3 @@
+from playsound import playsound
+
+playsound("song.mp3")
