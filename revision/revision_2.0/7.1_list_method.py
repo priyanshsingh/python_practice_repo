6118101d6fe1b101a3578857f06b944db156b1f1
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-22 20:49:42 +0530

    added list functions

--- revision/revision_2.0/7.1_list_method.py
+++ revision/revision_2.0/7.1_list_method.py
@@ -2,5 +2,19 @@ numbers = [13, 45, 878, 43, 11, 467]
 print(numbers)
 
 numbers.sort()
+print(numbers)
 
+numbers.reverse()
+print(numbers)
+
+numbers.append(456)
+print(numbers)
+
+numbers.insert(3,789)
+print(numbers)
+
+numbers.pop(2)
+print(numbers)
+
+numbers.remove(878)
 print(numbers)
