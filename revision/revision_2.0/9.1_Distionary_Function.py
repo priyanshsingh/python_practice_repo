e51f95703751695a701d2d55d3f7a7e47ebf4fa3
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-23 22:50:23 +0530

    added dictionary and its functions

---
+++ revision/revision_2.0/9.1_Distionary_Function.py
@@ -0,0 +1,19 @@
+dict1 = {"Name": "Priyansh",
+        "Marks" : "100",
+        "Age" : "21",
+        "list" : [1,2,3],
+        "dict2" : {"Name" : "Varun", "Surname" : "Singh"},
+        1: 5
+        }
+
+# print(dict1.keys())
+# print(type(dict1.keys()))
+# print(type(list(dict1.keys())))
+
+#print(dict1.values())
+
+#print(dict1.items())
+
+dict3 = {"Friend": "Aman"}
+dict1.update(dict3)
+print(dict1)
---
+++ revision/revision_2.0/9_Dictionary.py
@@ -0,0 +1,18 @@
+# Collection of key-value pairs
+
+dict1 = {"Name": "Priyansh",
+        "Marks" : "100",
+        "Age" : "21",
+        "list" : [1,2,3],
+        "dict2" : {"Name" : "Varun", "Surname" : "Singh"}
+        }
+print(dict1["Name"])
+print(dict1["Marks"]) 
+print(dict1["Age"])
+print(dict1["list"])
+print(dict1["dict2"]["Name"])
+
+dict1["Name"] = "Amit"
+print(dict1["Name"])
+
+ 
