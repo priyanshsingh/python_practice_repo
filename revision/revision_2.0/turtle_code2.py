b859b602273b87037ed39ca8635a370763858aa5
Priyansh Singh <64950658+priyanshsingh@users.noreply.github.com>
2021-12-24 03:06:07 +0530

    Create turtle_code2.py
    added a turtle code

---
+++ revision/revision_2.0/turtle_code2.py
@@ -0,0 +1,15 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+
+s.bgcolor("black")
+t.width(2)
+t.speed(15)
+col = ("#039fbe", "#e8d21d", "#cf1578")
+
+for i in range(1000):
+    t.pencolor(col[i%3])
+    t.forward(i*4)
+    t.right(121)
+
