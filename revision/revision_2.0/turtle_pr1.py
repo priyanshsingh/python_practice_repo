7a94aa2a23e021a1ff3ae7341547ec1c0f92a69f
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-27 00:29:46 +0530

    parcticed turtle and tkinter codes

---
+++ revision/revision_2.0/turtle_pr1.py
@@ -0,0 +1,35 @@
+import turtle
+
+s = turtle.getscreen()
+s.bgcolor("Black")
+
+t = turtle.Turtle()
+t.pencolor("white")
+
+t.speed(3)
+
+t.circle(50)
+
+t.rt(90)  
+t.circle(50)
+
+t.rt(180)  
+t.circle(50)
+
+t.rt(270)  
+t.circle(50)
+
+t.circle(70)
+
+t.rt(90)  
+t.circle(70)
+
+t.rt(180)  
+t.circle(70)
+
+t.rt(270)  
+t.circle(70)
+
+t.dot(50)
+
+turtle.mainloop()
