2b53bab8a93e2d1f0675bd6f7c5d01b7958b200e
priyanshsingh <priyanshsingh1401@gmail.com>
2021-12-27 19:21:32 +0530

    added code

---
+++ projects/tkinter/tkinter_revision/r1.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+priyansyh_root = Tk()
+
+priyansyh_root.geometry("680x480")
+
+priyansyh_root.maxsize(900, 800)
+priyansyh_root.minsize(320, 240)
+
+varun = Label(text="Hello, wassup bro!!!")
+varun.pack()
+
+priyansyh_root.mainloop()
