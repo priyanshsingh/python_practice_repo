7894eb9cad5ff13e7d6c6c13c510dba4e0855f03
priyanshsingh <priyanshsingh1401@gmail.com>
2022-01-06 17:27:19 +0530

    added prime num check

---
+++ revision/revision_2.0/14.4_sumOf_N_naturalNumbers.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number until which you want the addition tobe done: "))
+sum = 0
+i = 1
+while i<=num:
+    sum = sum+i
+    i+=1
+print("\nThe sum is = " + str(sum) + "\n") 
