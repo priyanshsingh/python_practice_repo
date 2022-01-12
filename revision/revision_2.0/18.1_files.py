ed08c56420afacb634426debc8b657b9e0e5f174
priyanshsingh <priyanshsingh1401@gmail.com>
2022-01-12 13:50:28 +0530

    added file read and write files

---
+++ revision/revision_2.0/18.1_files.py
@@ -0,0 +1,15 @@
+# by default the mode is 'r'
+f = open("sample.txt")
+data = f.readline()
+print(data)
+
+data = f.readline()
+print(data)
+
+data = f.readline()
+print(data)
+
+data = f.readline()
+print(data)
+
+f.close()
---
+++ revision/revision_2.0/18.2_writeToAFile.py
@@ -0,0 +1,3 @@
+f = open("hello.txt", "w")
+f.write("Hello world, wassup? How are you guys?")
+f.close()
